278d74830510f12d36164adafef0b9b92e71d06b
Aung Kyaw Thant <akthant@gmail.com>
2020-04-25 20:37:33 +0800

    First commit MEAN book-store

---
+++ src/app/book-create/book-create.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+@Component({
+  selector: 'app-book-create',
+  templateUrl: './book-create.component.html',
+  styleUrls: ['./book-create.component.css']
+})
+export class BookCreateComponent implements OnInit {
+  bookForm: FormGroup;
+  isbn:string='';
+  title:string='';
+  description:string='';
+  author:string='';
+  publisher:string='';
+  published_year:string='';
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
+
+  matcher = new MyErrorStateMatcher();
+
+  ngOnInit() {
+    this.bookForm = this.formBuilder.group({
+      'isbn' : [null, Validators.required],
+      'title' : [null, Validators.required],
+      'description' : [null, Validators.required],
+      'author' : [null, Validators.required],
+      'publisher' : [null, Validators.required],
+      'published_year' : [null, Validators.required]
+    });
+  }
+  onFormSubmit(form:NgForm) {
+    this.api.postBook(form)
+      .subscribe(res => {
+          let id = res['_id'];
+          this.router.navigate(['/book-details', id]);
+        }, (err) => {
+          console.log(err);
+        });
+  }
+}
+  /** Error when invalid control is dirty, touched, or submitted. */
+  export class MyErrorStateMatcher implements ErrorStateMatcher {
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+      const isSubmitted = form && form.submitted;
+      return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    }
+  }
